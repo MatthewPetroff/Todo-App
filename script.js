044ea2020108370b535df22cc8ecf1392f1030fb
Matt <matthewjpetroff@gmail.com>
2020-07-18 13:57:33 -0400

    Add script.js pseudocode

---
+++ script.js
@@ -0,0 +1,30 @@
+//Global Variables
+//let taskCount = 1;
+//let tasks = [];
+
+//Task Class
+
+	//TaskObject.id = `Task ${taskCount}`;
+	//TaskObject.description = value from user-input form
+	//TaskObject.isComplete = false;
+
+function update() {
+	//for item in task list:
+		//add task
+}
+
+function addTask() {
+	//instantiate class object to `task${taskCount}`, taskCount++
+	//add object to tasks list
+	//update html ul class='list'
+}
+
+function deleteTask() {
+	//delete object from tasks list
+	//update html ul class='list'
+}
+
+function completeTask() {
+	//set taskObject.isComplete to true
+	//update html ul class='list'
+}
